2bb3dc82b95f76a6ac487ce9073e18826567216b
IvanPrasetyoNugroho <ivanprasetyo559@gmail.com>
2022-03-15 21:25:45 +0700

    initial commit

---
+++ Rivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rivery
+//
+//  Created by MacBook Noob on 15/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
